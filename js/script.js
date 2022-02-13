884a4638ccadb33d1f5b10983a9034f6d48b850e
nxphgg16 <96096453+nxphgg16@users.noreply.github.com>
2022-02-13 07:16:29 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// slick slider=====================================
+$(document).ready(function(){
+    $('.home__slider').slick({
+        slidesToShow: 1,
+        infinite: true,
+        arrows: false,
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });  
+  });
+
+// slick slider=====================================  
+$(document).ready(function(){
+  $('.collection__slider').slick({
+      slidesToShow: 3,
+      infinite: true,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+  });  
+});
+
+// slick slider=====================================  
+$(document).ready(function(){
+  $('.destination__slider').slick({
+      slidesToShow: 1,
+      infinite: true,
+      arrows: false,
+      dots: false,
+      autoplay: true,
+      autoplaySpeed: 3000,
+  });  
+});
